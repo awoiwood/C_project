38acdea9b9d974b9b5d7e846a05d81756e2271f6
Ashton <awoiwood1@gmail.com>
2025-07-06 15:51:52 -0500

    Only accepts integers for age Uses getline for strings to cover cases where user types first and last name e.g. (john doe)

--- main.cpp
+++ main.cpp
@@ -1,23 +1,39 @@
 #include <iostream>
 #include <string>
 
+
+bool is_integer(const std::string& s) {
+    for (char ch : s) {
+        if (!std::isdigit(ch)) return false;
+    }
+    return !s.empty();
+}
+
 int main() {
     std::string name;
+    std::string input;
     int age;
     std::string fav_color;
 
     std::cout << "Enter your name: ";
     std::getline(std::cin, name);
 
-    std::cout << "Enter your age: ";
-    std::cin >> age;
-    std::cin.ignore();
+    while (true) {
+        std::cout << "Enter your age: ";
+        std::getline(std::cin, input);
+        if (is_integer(input)) {
+            age = std::stoi(input);
+            break;
+        } else {
+            std::cout << "Invalid input. Please enter a whole number.\n";
+        }
+    }
 
-    std::cout << "What is your favorite color?";
-    std::cin >> fav_color;
-    std::cin.ignore();
+    std::cout << "What is your favorite color? ";
+    std::getline(std::cin, fav_color);
 
-    std::cout << "Hello, " << name << "! You are " << age << " years old. " << "Your favorite color is " << fav_color << std::endl;
+    std::cout << "Hello, " << name << "! You are " << age << " years old. ";
+    std::cout << "Your favorite color is " << fav_color << "." << std::endl;
 
     return 0;
 }
